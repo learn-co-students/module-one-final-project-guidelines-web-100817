564e1c3529bc719aab99cbdb61280260b001e383
slin12 <s.lin12@gmail.com>
2017-10-23 16:47:02 -0400

    fix return output of pos/neg tweets

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,13 @@ class User < ActiveRecord::Base
   has_many :tweets
   has_many :tweet_hashtags, through: :tweets
   has_many :hashtags, through: :tweet_hashtags
+
+  def most_positive_tweet
+    Tweet.where("user_id = ?", self.id).order("sentiment_score DESC").first.content
+  end
+
+  def most_negative_tweet
+    Tweet.where("user_id = ?", self.id).order("sentiment_score ASC").first.content
+  end
+
 end
--- lib/sentiment.rb
+++ lib/sentiment.rb
@@ -41,6 +41,10 @@ class Sentiment
     end
   end
 
+  # def self.user_table
+  #
+  # end
+
   def self.most_positive_friend
     self.user_sentiment_hash.find {|name, score| score == self.user_sentiment_hash.values.max}[0]
   end
@@ -50,11 +54,15 @@ class Sentiment
   end
 
   def self.most_positive_tweet
-    Tweet.order("sentiment_score DESC").first.content
+    tweet = Tweet.order("sentiment_score DESC").first
+    user = User.find(tweet.user_id)
+    "#{user.name} said: #{tweet.content}"
   end
 
   def self.most_negative_tweet
-    Tweet.order("sentiment_score ASC").first.content
+    tweet = Tweet.order("sentiment_score ASC").first
+    user = User.find(tweet.user_id)
+    "@#{user.name} said: #{tweet.content}"
   end
 
 end
