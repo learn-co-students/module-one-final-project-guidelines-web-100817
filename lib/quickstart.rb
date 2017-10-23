a3e79328f680b6d409b6686b05b87e632a3429e5
slin12 <s.lin12@gmail.com>
2017-10-23 13:25:41 -0400

    merge

---
+++ lib/quickstart.rb
@@ -0,0 +1,19 @@
+# [START language_quickstart]
+# Imports the Google Cloud client library
+require "google/cloud/language"
+
+# Your Google Cloud Platform project ID
+project_id = "mod1-final-project"
+
+# Instantiates a client
+language = Google::Cloud::Language.new project: project_id
+
+# The text to analyze
+text     = "You suck!"
+document = language.document(text)
+# Detects the sentiment of the text
+sentiment = document.sentiment
+
+puts "Text: #{text}"
+puts "Score: #{sentiment.score}, #{sentiment.magnitude}"
+# [END language_quickstart]
