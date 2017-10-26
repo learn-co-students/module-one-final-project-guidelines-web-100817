80a187c5de7f97653ab3f50a0a2660cb5d9a02e4
slin12 <s.lin12@gmail.com>
2017-10-26 13:10:13 -0400

    add did you mean feature

--- bin/helper_methods/relations.rb
+++ bin/helper_methods/relations.rb
@@ -1,72 +1,86 @@
 ### RELATIONS ###
 def all_user_tweets(input)
   user = find_user(input)
-  puts "\nHere are all the tweets from #{user.name.light_green}:\n\n"
-  user.tweets.each do |tweet|
-    format_tweet(user, tweet)
+  if user
+    puts "\nHere are all the tweets from #{user.name.light_green}:\n\n"
+    user.tweets.each do |tweet|
+      format_tweet(user, tweet)
+    end
   end
 end
 
 def all_user_hashtags(input)
   user = find_user(input)
-  puts "\nHere are all the hashtags used by #{user.name.light_green}:\n\n"
-  user.hashtags.each do |hashtag|
-    puts "\##{hashtag.title}"
+  if user
+    puts "\nHere are all the hashtags used by #{user.name.light_green}:\n\n"
+    user.hashtags.each do |hashtag|
+      puts "\##{hashtag.title}"
+    end
+    puts ""
   end
-  puts ""
 end
 
 def all_hashtag_users(input)
-  binding.pry
   hashtag = find_hashtag(input)
-  puts "\nHere are all the people who have tweeted about #{"#".light_green}#{hashtag.title.light_green}:\n\n"
-  hashtag.users.uniq.each do |user|
-    puts user.name
+  if hashtag
+    puts "\nHere are all the people who have tweeted about #{"#".light_green}#{hashtag.title.light_green}:\n\n"
+    hashtag.users.uniq.each do |user|
+      puts user.name
+    end
+    puts ""
   end
-  puts ""
 end
 
 def all_hashtag_tweets(input)
   hashtag = find_hashtag(input)
-  puts "\nHere are all the tweets about \##{hashtag.title.light_green}:\n\n"
-  hashtag.tweets.each do |tweet|
-    user = User.find(tweet.user_id)
-    format_tweet(user, tweet)
+  if hashtag
+    puts "\nHere are all the tweets about \##{hashtag.title.light_green}:\n\n"
+    hashtag.tweets.each do |tweet|
+      user = User.find(tweet.user_id)
+      format_tweet(user, tweet)
+    end
   end
 end
 
 def user_top_tweets(input)
   user = find_user(input)
-  puts "\nHere are the top 5 tweets from #{user.name.light_green}:\n\n"
-  user.tweets.order("tweets.likes DESC").limit(5).each do |tweet|
-    format_tweet(user, tweet)
+  if user
+    puts "\nHere are the top 5 tweets from #{user.name.light_green}:\n\n"
+    user.tweets.order("tweets.likes DESC").limit(5).each do |tweet|
+      format_tweet(user, tweet)
+    end
   end
 end
 
 def user_top_hashtags(input)
   user = find_user(input)
-  binding.pry
-  puts "\nHere are hashtags most commonly used by #{user.name.light_green}:\n\n"
-  Hashtag.joins(:tweets).where("tweets.user_id = ?", user.id).group("hashtags.title").order("count(hashtags.title) DESC").each do |hashtag|
-    puts "\##{hashtag.title}: #{Hashtag.joins(:tweets).where("hashtags.id = #{hashtag.id}").count}"
+  if user
+    puts "\nHere are hashtags most commonly used by #{user.name.light_green}:\n\n"
+    Hashtag.joins(:tweets).where("tweets.user_id = ?", user.id).group("hashtags.title").order("count(hashtags.title) DESC").each do |hashtag|
+      puts "\##{hashtag.title}: #{Hashtag.joins(:tweets).where("hashtags.id = #{hashtag.id}").count}"
+    end
+    puts ""
   end
-  puts ""
 end
 
 def hashtag_top_tweets(input)
   hashtag = find_hashtag(input)
-  puts "\nHere are the 5 most popular tweets about \##{hashtag.title.light_green}:\n\n"
-  hashtag.tweets.order("tweets.likes DESC").limit(5).each do |tweet|
-    user = User.find(tweet.user_id)
-    format_tweet(user, tweet)
+  if hashtag
+    puts "\nHere are the 5 most popular tweets about \##{hashtag.title.light_green}:\n\n"
+    hashtag.tweets.order("tweets.likes DESC").limit(5).each do |tweet|
+      user = User.find(tweet.user_id)
+      format_tweet(user, tweet)
+    end
   end
 end
 
 def hashtag_top_users(input)
   hashtag = find_hashtag(input)
-  puts "\nHere are the people who tweet the most about #{"#".light_green}#{hashtag.title.light_green}"
-  User.joins(tweets: [:tweet_hashtags]).where("tweet_hashtags.hashtag_id = #{hashtag.id}").group("users.id").order("count(users.id) DESC").each do |user|
-    puts "#{user.name}: #{user.tweet_hashtags.where(hashtag_id: hashtag.id).count}"
+  if hashtag
+    puts "\nHere are the people who tweet the most about #{"#".light_green}#{hashtag.title.light_green}"
+    User.joins(tweets: [:tweet_hashtags]).where("tweet_hashtags.hashtag_id = #{hashtag.id}").group("users.id").order("count(users.id) DESC").each do |user|
+      puts "#{user.name}: #{user.tweet_hashtags.where(hashtag_id: hashtag.id).count}"
+    end
+    puts ""
   end
-  puts ""
 end
